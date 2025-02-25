d1e8d32ef67feb9e18ec3d1c153f995e81c3d5d6
ckkim <rlacksrb1274@naver.com>
2025-02-25 16:56:04 +0900

    어드민서비스 수정, 카테고리순서 변경

--- delivery/src/main/java/com/springboot/delivery/service/AdminService.java
+++ delivery/src/main/java/com/springboot/delivery/service/AdminService.java
@@ -3,24 +3,54 @@ package com.springboot.delivery.service;
 import java.util.List;
 import java.util.Locale.Category;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.springboot.delivery.mapper.AdminMapper;
 import com.springboot.delivery.model.User;
-
-import lombok.Getter;
-import lombok.Setter;
-
 import com.springboot.delivery.model.Coupon;
 import com.springboot.delivery.model.Maincategory;
 
-public interface AdminService {
-    List<User> getAllUsers();
-    List<Coupon> getAllCoupons();
-	List<Maincategory> getAllMaincategory();
-    void issueCouponToUser(String userId, Integer couponId);
-    void deleteUser(String userId);
-    void createCoupon(Coupon coupon);
-    void deleteCoupon(Integer cp_id);
-    void createMaincategory(Maincategory maincategory);
-    void deleteMaincategory(Integer main_category_id);
+@Service  
+public class AdminService{
+    
+    @Autowired
+    private AdminMapper adminMapper;
 
+    public void createCoupon(Coupon coupon) {
+        adminMapper.createCoupon(coupon);
+    }
+    public void createMaincategory(Maincategory maincategory) {
+        adminMapper.createMaincategory(maincategory);
+    }
+    public List<User> getAllUsers() {
+        return adminMapper.getAllUsers();
+    }
+
+    public List<Coupon> getAllCoupons() {
+        return adminMapper.getAllCoupons();
+    }
+    
+    public List<Maincategory> getAllMaincategory() {
+        return adminMapper.getAllMaincategory();
+    }
+
+
+    public void issueCouponToUser(String userId, Integer couponId) {
+        adminMapper.issueCouponToUser(userId, couponId);
+    }
+
+
+    public void deleteUser(String userId) {
+        adminMapper.deleteUser(userId);
+    }
+    
+
+    public void deleteCoupon(Integer cp_id) {
+        adminMapper.deleteCoupon(cp_id);
+    }
+    
+    public void deleteMaincategory(Integer main_category_id) {
+        adminMapper.deleteMaincategory(main_category_id);
+    }
+    
 }
-
